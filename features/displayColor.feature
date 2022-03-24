2852af16d0425ff347b34ff86a921a04d7839ab7
raf <rmalenta@poczta.wp>
2022-03-24 17:30:18 +0100

    set up behat

--- features/displayColor.feature
+++ features/displayColor.feature
@@ -3,8 +3,9 @@ Feature: Seeing prduct color
     I want to be see product color
 
     Scenario: Seeing color
-        Given There is product with slug "dress"
-        And has color "czerwony"
+        Given the store has locale "en-US"
+        And There is product with slug "dress" and color "czerwony"
+        And I am on "/dress"
         Then I should see "red"
 
 
--- tests/Behat/Contexts/ProductContext.php
+++ tests/Behat/Contexts/ProductContext.php
@@ -1,24 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Behat\Contexts;
 
+use App\Entity\Product;
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Product\Factory\ProductFactoryInterface;
 
-/**
- * Defines application features from the specific context.
- */
+
 class ProductContext extends MinkContext implements Context
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+
+        $this->entityManager = $entityManager;
     }
+    /**
+     * @Given There is product with slug :slug and color :color
+     */
+    public function thereIsProductWithSlugAndColor($slug, $color)
+    {
+
+        $product = new Product();
+        $product->setSlug($slug);
+        $product->setColor($color);
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+    }
+
+
 
 }
