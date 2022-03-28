58aaf90f0fde11fd1f2f797eabb13a1342533566
raf <rmalenta@poczta.wp>
2022-03-28 14:51:32 +0200

    fix ECS

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,19 +4,20 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 
 class Product extends BaseProduct implements ProductInterface
 {
-    protected const RED = "Czerwony";
-    protected const GREEN = "Zielony";
-    protected const BlUE = "Niebieski";
+    protected const RED = 'Czerwony';
+
+    protected const GREEN = 'Zielony';
+
+    protected const BlUE = 'Niebieski';
 
     protected ?string $color;
 
     /* @var array<string> */
-   public static array $colors = [
+    public static iterable $colors = [
         self::RED,
         self::GREEN,
         self::BlUE,
@@ -42,5 +43,4 @@ class Product extends BaseProduct implements ProductInterface
     {
         parent::__construct();
     }
-
 }
--- src/Form/Extension/ProductTypeExtension.php
+++ src/Form/Extension/ProductTypeExtension.php
@@ -4,31 +4,32 @@ declare(strict_types=1);
 
 namespace App\Form\Extension;
 
+use App\Entity\Product;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
-
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use App\Entity\Product;
-
 
 class ProductTypeExtension extends AbstractTypeExtension
 {
     /**
      * @param array<string, mixed> $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options ): void
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add("color", ChoiceType::class, [
-            'placeholder'=> "pick color",
-            'choice_loader' => new CallbackChoiceLoader(function() {
+        $builder->add(
+            'color',
+            ChoiceType::class,
+            [
+            'placeholder' => 'pick color',
+            'choice_loader' => new CallbackChoiceLoader(function () {
                 return Product::getColors();
             }),
             'choice_label' => function ($value) {
                 return $value;
             },
-            'translation_domain'=>'colors',
+            'translation_domain' => 'colors',
         ],
         );
     }
